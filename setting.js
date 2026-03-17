a68effa6eff2f4bfa9f866249b2ef00f955d6ce1
Kaizen <kaizendesu.my.id@gmail.com>
2026-03-18 01:20:42 +0700

    update setting.js

---
+++ setting.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+    BOT_TOKEN: isi,
+    BOT_NAME: "KaiBASE", 
+    OWNER_TELEGRAM_ID: isi,
+    OWNER_USERNAME: "kaizendesuid",
+    REQUIRED_GROUP_ID: -1003725619855,//CONTOH
+    REQUIRED_GROUP_LINK: "ISI",
+    OWNER_YOUTUBE: "",
+    OWNER_TIKTOK: "",
+    OWNER_INSTAGRAM: "",
+    CATBOX_API_URL: "",
+};
